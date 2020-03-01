0a3a2748450cc320a3288a1e8f105eee3828734e
ReCursia <saha2098@yandex.ru>
2020-03-01 16:00:01 +0300

    implementing

--- src/main/kotlin/models/Book.kt
+++ src/main/kotlin/models/Book.kt
@@ -2,6 +2,8 @@ package models
 
 import common.Htmlable
 
-data class Book(val name: String, val author: String, val isRead: Boolean = false) : Htmlable {
-    override fun toHtml() = "<tr><td><b>$author</b></td><td><b>$name</b></td><td><b>$isRead</b></td></tr>"
+data class Book(val name: String, val author: String, val isRead: Boolean = false, val curReader: String? = null) : Htmlable {
+    //TODO возможно всю чушь с html в отдельный класс, который будет генерировать данные с Instance
+    override fun toHtml() =
+        "<tr><td><b>$author</b></td><td><b>$name</b></td><td><b>$isRead</b><td><b>$curReader</b></td></tr>"
 }
--- src/main/kotlin/servlets/BooksListServlet.kt
+++ src/main/kotlin/servlets/BooksListServlet.kt
@@ -10,36 +10,32 @@ import javax.servlet.http.HttpServletResponse
 @WebServlet(name = "BooksList", value = ["/books"])
 class BooksListServlet : HttpServlet() {
     override fun doGet(req: HttpServletRequest, res: HttpServletResponse) {
-        val content = getTable()
-        res.contentType = "text/html;charset=UTF-8"
-        res.writer.write(content)
-    }
-
-    private fun getTable(): String {
-        return buildString {
-            append("<html>\n")
-            append("<head><title>Список книг</title></head>\n")
-            append("<body>\n")
-            append("<h1>Список книг читателя</h1>\n")
+        fun getTable() = buildString {
+            append("<html>")
+            append("<head><title>Список книг</title></head>")
+            append("<body>")
+            append("<h1>Список книг ${req.getParameter("name") ?: ""}</h1>")
             append("<table border='1'>\n")
-            append("<tr><td><b>Автор книги</b></td><td><b>Название книги </b></td><td><b>Прочитал</b></td></tr>\n")
-            getBooksFromFile().forEach { append(it.toHtml()) }
+            append("<tr><td><b>Автор книги</b></td><td><b>Название книги </b></td><td><b>Прочитал</b></td><td><b>Текущий читатель</b></td></tr>")
+            getBooksFromFile().filter { req.getParameter("name") == it.curReader }.forEach { append(it.toHtml()) }
             append("</table>")
             append("</body>")
             append("</html>")
         }
+        req.characterEncoding = "utf-8"
+        res.contentType = "text/html;charset=UTF-8"
+        res.writer.write(getTable())
     }
 
-    private fun getBooksFromFile(): List<Book> {
-        val books = mutableListOf<Book>()
+    private fun getBooksFromFile() = mutableListOf<Book>().apply {
         File(FILE_PATH).forEachLine {
             val splitted = it.split(' ')
             val name = splitted[0]
             val author = splitted[1]
             val isRead = splitted[2].toBoolean()
-            books.add(Book(name, author, isRead))
+            val curReader = if (splitted.size > 3) splitted[3] else null
+            add(Book(name, author, isRead, curReader))
         }
-        return books
     }
 
     companion object {
