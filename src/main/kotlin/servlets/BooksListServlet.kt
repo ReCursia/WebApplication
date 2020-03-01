03c2d5f5963e139685a14f625abf3b165714eb82
ReCursia <saha2098@yandex.ru>
2020-03-01 19:07:07 +0300

    implementing

--- src/main/kotlin/servlets/BooksListServlet.kt
+++ src/main/kotlin/servlets/BooksListServlet.kt
@@ -11,24 +11,26 @@ import javax.servlet.http.HttpServletResponse
 @WebServlet(name = "BooksList", value = ["/books"])
 class BooksListServlet : HttpServlet() {
     override fun doGet(req: HttpServletRequest, res: HttpServletResponse) {
+        req.characterEncoding = "utf-8"
+        val lang = req.getParameter("lang") ?: "en" //default lang is en
+        val name = req.getParameter("name")
         fun getTable() = buildString {
-            val titles = ResourceBundle.getBundle("Book", Locale(req.getParameter("lang")))
+            val titles = ResourceBundle.getBundle("Book", Locale(lang))
             append("<html>")
             append("<head><title>${titles.getString("title")}</title></head>")
             append("<body>")
-            append("<h1>${titles.getString("list")} ${req.getParameter("name") ?: ""}</h1>")
+            append("<h1>${titles.getString("list")} ${name ?: ""}</h1>")
             append("<table border='1'>")
             append("<tr>")
             append("<td><b>${titles.getString("name")}</b></td>")
             append("<td><b>${titles.getString("read")}</b></td>")
             append("<td><b>${titles.getString("author")}</b></td>")
             append("</tr>")
-            getBooksFromFile().filter { req.getParameter("name") == it.curReader }.forEach { append(it.toHtml()) }
+            getBooksFromFile().filter { name == it.curReader }.forEach { append(it.toHtml()) }
             append("</table>")
             append("</body>")
             append("</html>")
         }
-        req.characterEncoding = "utf-8"
         res.contentType = "text/html;charset=UTF-8"
         res.writer.write(getTable())
     }
